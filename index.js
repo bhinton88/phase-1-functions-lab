da34a3335313022e3c7a98e14723e4642e6709d2
Bobby Hinton <bob.g.hint@gmail.com>
2022-05-17 22:02:06 -0500

    commit tree 03cd600151ef77f5e61ddcadd36a6072967a4276

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const hqBlock = 42;
+const blockLength = 264;
+
+function distanceFromHqInBlocks (blockNumber) {
+    return Math.abs(blockNumber - hqBlock); 
+}
+
+function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * blockLength;
+}
+
+function distanceTravelledInFeet (start,destination) {
+    return Math.abs((destination - start) *  blockLength); 
+}
+
+function calculatesFarePrice(start,destination) {
+    const fareDistance = distanceTravelledInFeet(start,destination);
+    const baseDistance = 400;
+    if(fareDistance <= baseDistance) {
+        return 0; 
+    } else if (fareDistance <= 2000) {
+        return (fareDistance - baseDistance) * 0.02;  
+    } else if (fareDistance <= 2500) {
+        return 25;  
+    } else {
+        return `cannot travel that far`; 
+    }
+
+}
